fc08ff8245debddc0f27e7109872667d33faa778
Mohamed ElKady <Mohamed.ElKady@geidea.net>
2023-05-06 19:49:49 +0300

    todo entity

---
+++ TodoX.Domain/Entities/Todo.cs
@@ -0,0 +1,12 @@
+﻿using TodoX.Domain.Common;
+
+namespace TodoX.Domain.Entities
+{
+    public class Todo : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
